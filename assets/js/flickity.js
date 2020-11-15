7472ecc88cdee324f0961dfde197969c182d6379
Steffen Hoffman <steffen.hoffman@gmail.com>
2020-11-15 13:04:42 -0600

    again...

--- assets/js/flickity.js
+++ assets/js/flickity.js
@@ -3,31 +3,11 @@ var $carousel = $('.carousel').flickity({
 });
 
 // trigger play on all videos
-$carousel.find('video').each( function( i, video ) {
-  video.play();
-});
-
-// get Flickity instance
-var flkty = $carousel.data('flickity');
-
-function playOnSelect() {
-  var video = flickity.selectedElement.querySelector('video');
-  video.play();
-}
-
-// play initial video
-playOnSelect();
-// play video on select
-$carousel.on( 'select.flickity', playOnSelect );
+$carousel.find('video').get(0).play();
 
 $('.carousel-nav-buttons').on( 'click', '.nav-item', function() {
   var index = $(this).index();
   $carousel.flickity( 'select', index );
   $(".nav-item").removeClass("active");
   $(this).addClass("active");
-});
-
-
-$( function() {
-
 });
