3aed7b747f9c9d0635f50e3e018ed44f69da4909
Steffen Hoffman <steffen.hoffman@gmail.com>
2020-11-15 13:02:51 -0600

    Update

--- assets/js/flickity.js
+++ assets/js/flickity.js
@@ -2,30 +2,32 @@ var $carousel = $('.carousel').flickity({
   prevNextButtons: false,
 });
 
+// trigger play on all videos
+$carousel.find('video').each( function( i, video ) {
+  video.play();
+});
+
+// get Flickity instance
+var flkty = $carousel.data('flickity');
+
+function playOnSelect() {
+  var video = flickity.selectedElement.querySelector('video');
+  video.play();
+}
+
+// play initial video
+playOnSelect();
+// play video on select
+$carousel.on( 'select.flickity', playOnSelect );
+
+$('.carousel-nav-buttons').on( 'click', '.nav-item', function() {
+  var index = $(this).index();
+  $carousel.flickity( 'select', index );
+  $(".nav-item").removeClass("active");
+  $(this).addClass("active");
+});
+
 
 $( function() {
-  // trigger play on all videos
-  $carousel.find('video').each( function( i, video ) {
-    video.play();
-  });
 
-  // get Flickity instance
-  var flkty = $carousel.data('flickity');
-
-  function playOnSelect() {
-    var video = flickity.selectedElement.querySelector('video');
-    video.play();
-  }
-
-  // play initial video
-  playOnSelect();
-  // play video on select
-  $carousel.on( 'select.flickity', playOnSelect );
-
-  $('.carousel-nav-buttons').on( 'click', '.nav-item', function() {
-    var index = $(this).index();
-    $carousel.flickity( 'select', index );
-    $(".nav-item").removeClass("active");
-    $(this).addClass("active");
-  });
 });
