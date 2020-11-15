8eab8dbec8291328465cb0004961bd0ac59df6e1
Steffen Hoffman <steffen.hoffman@gmail.com>
2020-11-15 16:44:48 -0600

    updated flickity

--- assets/js/flickity.js
+++ assets/js/flickity.js
@@ -2,11 +2,6 @@ var $carousel = $('.carousel').flickity({
   prevNextButtons: false,
 });
 
-// trigger play on all videos
-$carousel.find('video').each( function( i, video ) {
-  video.get(0).play();
-});
-
 $('.carousel-nav-buttons').on( 'click', '.nav-item', function() {
   var index = $(this).index();
   $carousel.flickity( 'select', index );
